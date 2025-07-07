f37c17ee7c0da6c7b673cec9f10bf548348314af
013Ratchanon <Wuttha@webmail.npru.ac.th>
2025-07-07 11:08:12 +0700

    up

--- src/GenerateBill.java
+++ src/GenerateBill.java
@@ -7,18 +7,15 @@ public class GenerateBill {
         GetPlanFactory planFactory = new GetPlanFactory();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        
         System.out.print("Enter the name of plan for which the bill will be generated: ");
         String planName = br.readLine();
 
-      
         System.out.print("Enter the number of units for which the bill will be calculated: ");
         int units = Integer.parseInt(br.readLine());
 
-        
         Plan p = planFactory.getPlan(planName);
 
-       
+        // .
         if (p != null) {
             p.getRate();
             System.out.print("Bill amount for " + planName + " of " + units + " units is: ");
